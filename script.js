7ebd513c5b6dfb7202b88ef7dca2b244e45a934a
felachek <felachek@gmail.com>
2021-10-31 18:35:52 -0400

    init cy - fix cube rotation - shuffling

--- script.js
+++ script.js
@@ -1,159 +1,167 @@
 import "./style.css";
-import * as THREE from "three";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import * as dat from "dat.gui";
+// import * as THREE from "three";
+// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-// Debug
-const gui = new dat.GUI();
+//canvas
+var element = document.getElementById("scene");
 
-// Canvas
-const canvas = document.querySelector("canvas.webgl");
+var scene = new THREE.Scene(),
+  camera = new THREE.PerspectiveCamera(
+    20,
+    element.offsetWidth / element.offsetHeight,
+    0.1,
+    2000
+  ),
+  renderer = new THREE.WebGLRenderer({ antialias: true });
 
-// Scene
-const scene = new THREE.Scene();
+renderer.setClearColor(0xeeeeee, 1.0);
+renderer.setSize(element.offsetWidth, element.offsetHeight);
 
+camera.position.set(-30, 40, 30);
+camera.lookAt(scene.position);
+// var controls = new OrbitControls(camera, renderer.domElement);
+var controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.addEventListener("change", render);
+
+scene.add(new THREE.AxisHelper(20));
+scene.add(new THREE.AmbientLight(0xffffff));
+element.append(renderer.domElement);
+
+//Cube geometry
 var cubeSize = 3,
   dimensions = 3,
   spacing = 0.5;
 
-var colours = [
-  0xfffa41, //yellow
-  0x8541ff, //purple
-  0xff9800, //orange
-  0x00c3ff, //blue
-  0xfd0096, //pink
-  0x49ff00, //green
-];
-var faceMaterials = colours.map(function (c) {
-  return new THREE.MeshLambertMaterial({ color: c, emissive: c });
-});
+//Cube materials
+var colours = [0xc41e3a, 0x009e60, 0x0051ba, 0xff5800, 0xffd500, 0xffffff],
+  faceMaterials = colours.map(function (c) {
+    return new THREE.MeshLambertMaterial({ color: c, ambient: c });
+  });
+var cubeMaterials = new THREE.MeshFaceMaterial(faceMaterials);
 
-var cubeGeometry = new THREE.BoxBufferGeometry(
-  cubeSize,
-  cubeSize,
-  cubeSize
-).toNonIndexed();
+//Cube construction
+var allCubes = [],
+  pivot = new THREE.Object3D();
 
-function createCube(x, y, z) {
-  var cube = new THREE.Mesh(cubeGeometry, faceMaterials);
-  cube.castShadow = true;
-  cube.name = x + "-" + y + "-" + z;
-  console.log(x + "-" + y + "-" + z)
-  cube.position.x = x;
-  cube.position.y = y;
-  cube.position.z = z;
-  scene.add(cube);
+function newCube(x, y, z) {
+  var cubeGeometry = new THREE.CubeGeometry(cubeSize, cubeSize, cubeSize),
+    //   var cubeGeometry = new THREE.BoxBufferGeometry(cubeSize, cubeSize, cubeSize),
+    cube = new THREE.Mesh(cubeGeometry, cubeMaterials);
+  cube.position.set(x, y, z), scene.add(cube);
+  allCubes.push(cube);
 }
 
-//create cubes
+//Create the cubes
 var increment = cubeSize + spacing;
-
 for (var i = 0; i < dimensions; i++) {
   for (var j = 0; j < dimensions; j++) {
     for (var k = 0; k < dimensions; k++) {
-      createCube(i * increment, j * increment, k * increment);
+      var x = (i - 1) * increment,
+        y = (j - 1) * increment,
+        z = (k - 1) * increment;
+      newCube(x, y, z);
     }
   }
 }
 
-// Lights
-const pointLight = new THREE.PointLight(0xffffff, 0.1);
-pointLight.position.x = 2;
-pointLight.position.y = 3;
-pointLight.position.z = 4;
-scene.add(pointLight);
-
-/**
- * Sizes
- */
-const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
-};
-
-window.addEventListener("resize", () => {
-  // Update sizes
-  sizes.width = window.innerWidth;
-  sizes.height = window.innerHeight;
-
-  // Update camera
-  camera.aspect = sizes.width / sizes.height;
-  camera.updateProjectionMatrix();
-
-  // Update renderer
-  renderer.setSize(sizes.width, sizes.height);
-  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-});
-
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(
-  80,
-  sizes.width / sizes.height,
-  0.1,
-  100
-);
-
-camera.position.x = 15;
-camera.position.y = 10;
-camera.position.z = 15;
-scene.add(camera);
-
-// Controls
-const controls = new OrbitControls(camera, canvas);
-controls.enableDamping = true;
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-});
-//size of canvas
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-/**
- * Animate
- */
-const clock = new THREE.Clock();
-
-const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
-
-  // Update Orbital Controls
-  controls.update();
-
-  // Render
+function render() {
   renderer.render(scene, camera);
-  // console.log(scene)
-  // mesh.rotation.x = Math.PI / 2;
-
-  // Call tick again on the next frame
-  window.requestAnimationFrame(tick);
-};
-
-tick();
-
-function buttonFunction() {
-  scene.traverse(function(child) {
-      let name = child.name.split('-')
-      console.log(name[0])
-    if (name[0] === "0") {
-      child.rotation.x += Math.PI / 2
-    }
-  });
 }
 
+//Move all cubes with value 'v' on axis 'axis'
+function move(v, axis, rotation) {
+  var activeCubes = [];
 
-var params = {
-  ClickMe: buttonFunction
-};
+  allCubes.forEach(function (cube) {
+    var cubePosition = cube.position.clone();
 
-var folder = gui.addFolder('Rotation');
+    if (Math.abs(cubePosition[axis] - v) < 0.0001) activeCubes.push(cube);
+  });
 
-folder.add(params, 'ClickMe');
+  pivot.rotation.set(0, 0, 0);
+  pivot.updateMatrixWorld();
+  console.log(pivot);
 
-folder.open();
+  activeCubes.forEach(function (cube) {
+    THREE.SceneUtils.attach(cube, scene, pivot);
+    // pivot.add(cube);
+    // scene.add(pivot);
+  });
+
+  pivot.rotation[axis] = rotation;
+  pivot.updateMatrixWorld();
+  //   pivot.children = []
+  //   pivot.clear()
+  activeCubes.forEach(function (cube) {
+    cube.updateMatrixWorld();
+    THREE.SceneUtils.detach(cube, pivot, scene);
+    // scene.remove(pivot);
+    // pivot.remove(cube);
+  });
+
+  render();
+}
+
+//Render the initial state
+render();
+
+const counterClockwise = -Math.PI / 2;
+const clockwise = Math.PI / 2;
+
+//Pre-canned demo moves
+document.getElementById("moveY0").addEventListener("click", function () {
+  move(3.5, "y", clockwise);
+});
+document.getElementById("moveY1").addEventListener("click", function () {
+  move(0, "y", clockwise);
+});
+document.getElementById("moveY2").addEventListener("click", function () {
+  move(-3.5, "y", clockwise);
+});
+document.getElementById("moveX0").addEventListener("click", function () {
+  move(3.5, "x", clockwise);
+});
+document.getElementById("moveX1").addEventListener("click", function () {
+  move(0, "x", clockwise);
+});
+document.getElementById("moveX2").addEventListener("click", function () {
+  move(-3.5, "x", clockwise);
+});
+document.getElementById("moveZ0").addEventListener("click", function () {
+  move(3.5, "z", clockwise);
+});
+document.getElementById("moveZ1").addEventListener("click", function () {
+  move(0, "z", clockwise);
+});
+document.getElementById("moveZ2").addEventListener("click", function () {
+  move(-3.5, "z", clockwise);
+});
+
+//shuffle part
+function getArrayRandomElement(arr) {
+  if (arr && arr.length) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+}
+function shuffle(v, axis, rotation, i) {
+  setTimeout(function () {
+    move(v, axis, rotation);
+  }, 200 * i);
+}
+document.getElementById("shuffle").addEventListener("click", function () {
+  const axis = ["x", "y", "z"];
+  const v = [-3.5, 0, 3.5];
+  const rotation = [clockwise, counterClockwise];
+  for (let i = 0; i < 20; i++) {
+    shuffle(
+      getArrayRandomElement(v),
+      getArrayRandomElement(axis),
+      getArrayRandomElement(rotation),
+      i
+    );
+  }
+});
+
+document.getElementById("solve").addEventListener("click", function () {
+  move(-3.5, "z", counterClockwise);
+});
